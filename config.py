a5e71132dd7d271112cd3606ee942e541761b94d
ayanmiyana2 <ayan.miyana2@gmail.com>
2025-08-19 00:27:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = "8498286596:AAGtY1kbrXnKZMTQuw0x2KL7gOb5AKAC_TI"  # apna bot token yahan daalo
+TOKEN = "8047724576:AAH5sS-yuJXO24WMM3cz75RAUSROi2Z4d4E"  # apna bot token yahan daalo
 ADMIN_ID = 6324825537      # apna Telegram user id (int)
