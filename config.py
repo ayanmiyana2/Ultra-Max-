f30a3195ac47b65ee0557d02c4fb030b9fa0bf80
ayanmiyana2 <ayan.miyana2@gmail.com>
2025-08-19 00:12:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = "YOUR_BOT_TOKEN"  # apna bot token yahan daalo
-ADMIN_ID = 123456789      # apna Telegram user id (int)
+TOKEN = "8498286596:AAGtY1kbrXnKZMTQuw0x2KL7gOb5AKAC_TI"  # apna bot token yahan daalo
+ADMIN_ID = 6324825537      # apna Telegram user id (int)
